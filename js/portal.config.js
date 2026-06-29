ff218a6ad8e2a84c3290057cb8a4f19669fa79f4
Saad Shaikh <ss.saad148@gmail.com>
2026-06-30 00:26:29 +0100

    feat: invite button + modal, fix status constraint

--- js/portal.config.js
+++ js/portal.config.js
@@ -2,4 +2,5 @@
 const PORTAL_CONFIG = {
   supabaseUrl:  'https://ssbcpblfkgpgtcxifopp.supabase.co',
   supabaseAnon: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzYmNwYmxma2dwZ3RjeGlmb3BwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MzMxMTEsImV4cCI6MjA5ODMwOTExMX0.aAdKVdtWJHEFkFbmxlymENzXmGCZXJOoH4mgyCjXcKY',
+  edgeBase:     'https://ssbcpblfkgpgtcxifopp.supabase.co/functions/v1',
 };
